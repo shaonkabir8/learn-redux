e70e7ea763c344cacaa1f5da6350b36bc289afa5
Shaonkabir <shaonkabir98@gmail.com>
2020-01-28 14:14:05 +0600

    Feat: set React Router with Nested Routing for Reduxtagram

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -2,10 +2,27 @@ import React from 'react';
 
 import { render } from 'react-dom';
 
+// Import Router
+import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+
 // Import CSS
 import css from './styles/style.styl';
 
 // Import Components
 import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
 
-render(<Main />, document.querySelector('#root'));
+
+// Control Routing
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+
+render(router, document.querySelector('#root'));
