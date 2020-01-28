ad8ce5ff95484e7e691300ed35ab8dcf899ea469
Shaonkabir <shaonkabir98@gmail.com>
2020-01-29 00:55:46 +0600

    Feat: add Reducers

---
+++ client/reducers/rootReducer.js
@@ -0,0 +1,17 @@
+import {combineReducers} from 'redux'
+import {routerReducer} from 'react-router-redux';
+
+
+// Import other Reducers to combine
+import posts from './posts';
+import comments from './comments';
+
+
+// create a root reducer for creating store
+const rootReducer = combineReducers({
+	posts,
+	comments,
+	router: routerReducer
+});
+
+export default rootReducer;
